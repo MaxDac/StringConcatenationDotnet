ced970d52eb89a29551637e157223ff6b5380d08
Massimiliano D'Acunzo <mdacunzo@microsoft.com>
2022-11-25 10:46:59 +0000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<Benchy>();
+
+		}
+	}
+
+	[MemoryDiagnoser]
+	public class Benchy
+	{
+		[Benchmark]
+		public string Add2Strings()
+		{
+			string tenantId = "SysTenant";
+			string orgId = "ORGCode";
+			return tenantId + orgId;
+		}
+
+		[Benchmark]
+		public string Join2Strings()
+		{
+			string tenantId = "SysTenant";
+			string orgId = "ORGCode";
+			return string.Join("", tenantId, orgId);
+		}
+
+		[Benchmark]
+		public string Concat2StringAsSpan()
+		{
+			string tenantId = "SysTenant";
+			string orgId = "ORGCode";
+			return string.Concat(tenantId.AsSpan(), orgId.AsSpan());
+		}
+
+		[Benchmark]
+		public string Add2StringsWithCreate()
+		{
+			string tenantId = "SysTenant";
+			string orgId = "ORGCode";
+			int length = tenantId.Length + orgId.Length;
+			var context = ValueTuple.Create(tenantId, orgId);
+			var myString = string.Create(length, context, (span, state) =>
+			{
+				int index = state.Item1.Length;
+				state.Item1.AsSpan().CopyTo(span);
+				//state.Item2.AsSpan().CopyTo(span.Slice(index, state.Item2.Length));
+
+			});
+			return myString;
+		}
+	}
+}
